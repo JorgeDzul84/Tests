b282eb9634caa446bfc960f8cef7fc5d4d286543
JorgeDzul84 <jorge.dzulgilethought.com>
2023-03-08 11:26:08 -0600

    adding complementation

--- src/main/java/com/example/demo/controllers/UsuarioController.java
+++ src/main/java/com/example/demo/controllers/UsuarioController.java
@@ -1,11 +1,55 @@
 package com.example.demo.controllers;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.service.annotation.DeleteExchange;
 
 import com.example.demo.models.UsuarioModel;
+import com.example.demo.services.UsuarioService;
 
-@Repository
-public interface UsuarioController extends CrudRepository<UsuarioModel, Long> {
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController  {
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping()
+    public ArrayList<UsuarioModel> obtenerUusaurios(){
+        return usuarioService.obtenerUsuarios();
+    }
     
+    @PostMapping()
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usaurio){
+        return this.usuarioService.guardarUsuario(usaurio);
+    }
+
+    @GetMapping( path= "/{id}")
+    public Optional<UsuarioModel> obtenerUsuarioPorId(@PathVariable("id") Long id){
+        return this.usuarioService.obtenerPorId(id);
+    }
+
+    @GetMapping("/query")
+    public ArrayList<UsuarioModel> obtenerUsuarioPorPrioridad(@RequestParam("prioridad") Integer prioridad){
+        return this.usuarioService.obtenerPorPrioridad(prioridad);
+    }
+
+    @DeleteMapping(path="/{id}")
+    public String eliminarPorId(@PathVariable("id") Long id){
+        boolean ok = this.usuarioService.eliminarUsuario(id);
+        if (ok){
+            return "Se eliminó el usuario con id " + id;
+        }else{
+            return " No se pudo eliminar el usuario con id" + id;
+        }
+    }
 }
--- src/main/java/com/example/demo/repositories/UsuarioRepository.java
+++ src/main/java/com/example/demo/repositories/UsuarioRepository.java
@@ -1,5 +1,14 @@
 package com.example.demo.repositories;
 
-public class UsuarioRepository {
+import java.util.ArrayList;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.UsuarioModel;
+
+@Repository
+public interface UsuarioRepository extends CrudRepository<UsuarioModel, Long>{
+    public abstract ArrayList<UsuarioModel> findByPrioridad(Integer prioridad);
     
 }
